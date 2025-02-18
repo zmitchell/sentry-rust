459d075b15b203eadd18830805cd6104779fffd5
Zach Mitchell <zmitchell@fastmail.com>
2025-02-17 20:26:32 -0700

    feat: add feedback api

--- sentry-types/src/protocol/v7.rs
+++ sentry-types/src/protocol/v7.rs
@@ -26,6 +26,7 @@ use crate::utils::{display_from_str_opt, ts_rfc3339_opt, ts_seconds_float};
 
 pub use super::attachment::*;
 pub use super::envelope::*;
+pub use super::feedback::*;
 pub use super::monitor::*;
 pub use super::session::*;
 
@@ -1100,6 +1101,8 @@ pub enum Context {
     Gpu(Box<GpuContext>),
     /// OpenTelemetry data.
     Otel(Box<OtelContext>),
+    /// User feedback
+    Feedback(Box<Feedback>),
     /// Generic other context data.
     #[serde(rename = "unknown")]
     Other(Map<String, Value>),
@@ -1117,6 +1120,7 @@ impl Context {
             Context::Trace(..) => "trace",
             Context::Gpu(..) => "gpu",
             Context::Otel(..) => "otel",
+            Context::Feedback(..) => "feedback",
             Context::Other(..) => "unknown",
         }
     }
@@ -1690,6 +1694,9 @@ pub struct Event<'a> {
     /// SDK metadata
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub sdk: Option<Cow<'a, ClientSdkInfo>>,
+    /// The event type
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub r#type: Option<String>,
 }
 
 impl Default for Event<'_> {
@@ -1722,6 +1729,7 @@ impl Default for Event<'_> {
             extra: Default::default(),
             debug_meta: Default::default(),
             sdk: Default::default(),
+            r#type: Default::default(),
         }
     }
 }
@@ -1767,6 +1775,7 @@ impl<'a> Event<'a> {
             extra: self.extra,
             debug_meta: Cow::Owned(self.debug_meta.into_owned()),
             sdk: self.sdk.map(|x| Cow::Owned(x.into_owned())),
+            r#type: self.r#type,
         }
     }
 }
